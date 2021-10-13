6bcb3ffc88f34d813d38620e4bf4c425335a4b55
parth-d <pdeshp8@uic.edu>
2021-10-12 21:20:24 -0500

    - mr2 sorting done

--- src/main/scala/com/parth/scala/mr2.scala
+++ src/main/scala/com/parth/scala/mr2.scala
@@ -3,7 +3,7 @@ package com.parth.scala
 import org.apache.commons.beanutils.converters.DateTimeConverter
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.{IntWritable, Text, Writable}
+import org.apache.hadoop.io.{IntWritable, Text, Writable, WritableComparable, WritableComparator}
 import org.apache.hadoop.mapred.join.TupleWritable
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
@@ -56,28 +56,67 @@ object mr2 {
     }
   }
 
+  class comparator extends WritableComparator(classOf[IntWritable], true){
+    @SuppressWarnings(Array("rawtypes"))
+    override def compare(a: WritableComparable[_], b: WritableComparable[_]): Int = {
+      val k1 = a.asInstanceOf[IntWritable]
+      val k2 = b.asInstanceOf[IntWritable]
+      -1 * k1.compareTo(k2)
+    }
+  }
+
+  class InverseMapper extends Mapper[Object, Text, IntWritable, Text]{
+    override def map(key: Object, value: Text, context: Mapper[Object, Text, IntWritable, Text]#Context): Unit ={
+//      val values = value.toString.split("\\s+")
+//      context.write(new Text(values(1)), new IntWritable(values(0).toInt))
+      val pattern = Pattern.compile("(.*)\\s+(.*)")
+      val matcher = pattern.matcher(value.toString)
+      if (matcher.find())
+        context.write(new IntWritable(matcher.group(2).toInt), new Text(matcher.group(1)))
+    }
+  }
+
+  class SortReducer extends Reducer[IntWritable, Text, IntWritable, Text]{
+    override def reduce(key: IntWritable, values: Iterable[Text], context: Reducer[IntWritable, Text, IntWritable, Text]#Context): Unit = {
+      values.forEach(text => {
+        context.write(key, text)
+      })
+    }
+  }
+
+
 
   def main(args: Array[String]): Unit = {
     val configuration = new Configuration
     // Add config file to store global regex
     val pattern_match = "([a-c][e-g][0-3]|[A-Z][5-9][f-w]){5,15}"
-    configuration.set("Interval", args(2))
+    configuration.set("Interval", args(3))
     configuration.set("pattern_match", pattern_match)
     import org.apache.hadoop.fs.FileSystem
     val fs = FileSystem.get(configuration)
     if (fs.exists(new Path(args(1)))) fs.delete(new Path(args(1)), true)
-    val job = Job.getInstance(configuration,"word count")
+    val job = Job.getInstance(configuration, "word count")
     job.setJarByClass(this.getClass)
     job.setMapperClass(classOf[TokenizerMapper])
     job.setCombinerClass(classOf[IntSumReader])
     job.setReducerClass(classOf[IntSumReader])
     job.setOutputKeyClass(classOf[Text])
-    job.setOutputKeyClass(classOf[Text]);
-    job.setOutputValueClass(classOf[IntWritable]);
+    job.setOutputValueClass(classOf[IntWritable])
     FileInputFormat.addInputPath(job, new Path(args(0)))
     FileOutputFormat.setOutputPath(job, new Path(args(1)))
+    if (job.waitForCompletion(true)) {
 
-    System.exit(if(job.waitForCompletion(true))  0 else 1)
+      if (fs.exists(new Path(args(2)))) fs.delete(new Path(args(2)), true)
+      val sortjob = Job.getInstance()
+      sortjob.setJarByClass(this.getClass)
+      sortjob.setMapperClass(classOf[InverseMapper])
+      sortjob.setReducerClass(classOf[SortReducer])
+      sortjob.setSortComparatorClass(classOf[comparator])
+      sortjob.setOutputKeyClass(classOf[IntWritable])
+      sortjob.setOutputValueClass(classOf[Text])
+      FileInputFormat.addInputPath(sortjob, new Path(args(1)))
+      FileOutputFormat.setOutputPath(sortjob, new Path(args(2)))
+      System.exit(if (sortjob.waitForCompletion(true)) 0 else 1)
+    }
   }
-
 }
